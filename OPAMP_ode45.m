5e79048c0fa24c0571fb6cf39a88a65b0ce8f13e
Elburguer <142627932+Elburguer@users.noreply.github.com>
2023-10-08 21:03:47 -0600

    Update OPAMP_ode45.m

--- OPAMP_ode45.m
+++ OPAMP_ode45.m
@@ -1,4 +1,11 @@
-[t,x]=ode45(@OPAMP, [0 0.003], [0 0]); 
-figure;
-plot(t,x(:,1))
+%-----Función ode45 que resuslve numericamente el sistema------%
+[t,x]=ode45(@OPAMP, [0 2], [0 0]);
+%Graficamos nuestro sistemas de ecuaciaones
+% x y t son parametros que retorna la función que creamos
+%Figura 1
+figure(1)
+plot(t,x(:,1),'R');
 grid on
+title("Volatej de salida");
+xlabel("Tiempo");
+ylabel("Voltaje");
