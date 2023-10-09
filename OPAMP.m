213bd02293bc21c6e6a93c03666c17a4496e29b5
Elburguer <142627932+Elburguer@users.noreply.github.com>
2023-10-08 21:03:44 -0600

    Update OPAMP.m

--- OPAMP.m
+++ OPAMP.m
@@ -1,17 +1,16 @@
 function dx=OPAMP(t,x)
-%-----Definicion de los parametros-----%
-R1=1000;
-R2=1000;
-R3=10000;
-R4=15000;
-C1=0.1e-6;
-C2=0.1e-6;
-v7=1;
-%ciclo de trabajo
-%--------------------------------------%
-dx = zeros(2,1);
-%-----Definicion de la dinamica del sistema-----%
-%k=(R3+R4)/(R3*x(1));
-dx(1) = x(2);
-dx(2) = (((R3+R4)*v7)-((R1*R3*C1)+(R2*R3*C2)+(R1*R3*C2)-(R3*R1*C1+R4*R1*C1))*x(2)-R3*x(1))/(R1*R2*C1*C2*R3);
+%Función del amplificador operacional
+%-----Definimos nuestro parametros------%
+R1=5e6; %Resistencia 1 (Ohms)
+R2=5e6; %Resistencia 2 (Ohms)
+R3=5e6; %Resistencia 3 (Ohms)
+C1=100e-9; %Capacitor 1 (F)
+C2=100e-9; %Capacitor 2 (F)
+v=1; %Voltaje de entrada (v)
+%------Creamos nuestra matriz de las variables de estado----%
+dx=zeros(2,1);
+%-----Definición dinamica del sistema (Representanción de estados)-----%
+dx(1)=x(2);
+dx(2)=(1/(R1*R2*R3*C1*C2))*(R2*v-R1*R3*C2*x(2));
+
 %--------------------------------------%
